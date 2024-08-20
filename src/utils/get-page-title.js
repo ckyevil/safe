2f6e2f9bf7eb010b5a180c0e3ae8391d694ede97
ckyevil <130055260+ckyevil@users.noreply.github.com>
2024-08-20 15:27:25 +0800

    Update get-page-title.js

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || '安全星盟'
+const title = defaultSettings.title || '隐私星链'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
