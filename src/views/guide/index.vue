263b2e0c9b11c3473e535524937c546000391302
ckyevil <130055260+ckyevil@users.noreply.github.com>
2024-08-20 15:31:51 +0800

    Update index.vue

--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <aside>
-      这是对于安全星盟系统使用说明，我们将在这里展示我们的项目导航功能，感谢您支持并使用我们的系统
+      这是对于隐私星链系统使用说明，我们将在这里展示我们的项目导航功能，感谢您支持并使用我们的系统
       <!-- <a href="https://github.com/kamranahmedse/driver.js" target="_blank">driver.js.</a> -->
     </aside>
     <el-button icon="el-icon-question" type="primary" @click.prevent.stop="guide">
